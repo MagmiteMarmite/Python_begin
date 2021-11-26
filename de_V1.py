5064fced747078313261d5577b362ef0488e2531
MagmiteMarmite <95070892+MagmiteMarmite@users.noreply.github.com>
2021-11-26 21:10:54 +0100

    Delete de_V1.py

--- de_V1.py
+++
@@ -1,7 +0,0 @@
-import random
-
-print("Bienvenue dans mon petit jeux")
-print("La règle est simple vous participerez à 6 niveau votre but GAGNER")
-
-user = input('Quel est votre nom, futur champion ?\n')
-alea = random.randint(1,6)
